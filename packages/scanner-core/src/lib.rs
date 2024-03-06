c1ee6981bb7176e367ea416339f490b42c5ff560
Luca Georges Francois <luca@quartz.technology>
2024-03-06 21:48:27 +0100

    feat(scanner-core): add methods placeholders
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- packages/scanner-core/src/lib.rs
+++ packages/scanner-core/src/lib.rs
@@ -1,5 +1,8 @@
 extern crate wasm_bindgen;
 
+use std::u8;
+use wasmprinter::print_bytes;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,4 +13,28 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, World!");
+}
+
+#[wasm_bindgen]
+pub fn scan(input: &[u8]) {
+    // let wat = print_file("index.wasm").unwrap();
+    // let wat2 = wat::parse_str(&wat).unwrap();
+
+    let wat = print_bytes(input).unwrap();
+    let _wat2 = wat::parse_str(&wat).unwrap();
+}
+
+#[wasm_bindgen]
+pub fn scan_host_functions(_input: &[u8]) {
+    alert("Scanning host functions...");
+}
+
+#[wasm_bindgen]
+pub fn scan_exported_function_names(_input: &[u8]) {
+    alert("Scanning exported function names...");
+}
+
+#[wasm_bindgen]
+pub fn scan_constants(_input: &[u8]) {
+    alert("Scanning constants...");
 }
