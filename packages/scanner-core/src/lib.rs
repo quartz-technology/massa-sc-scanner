99c8239ecd4fe16790a4bf03cd794f30963c1ad7
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-03-06 16:54:49 -0500

    feat(core): scan exported function names

--- packages/scanner-core/src/lib.rs
+++ packages/scanner-core/src/lib.rs
@@ -1,9 +1,11 @@
 extern crate wasm_bindgen;
 
+use std::vec;
 use std::u8;
 use wasmprinter::print_bytes;
 
 use wasm_bindgen::prelude::*;
+use wasmparser::*;
 
 #[wasm_bindgen]
 extern {
@@ -30,8 +32,36 @@ pub fn scan_host_functions(_input: &[u8]) {
 }
 
 #[wasm_bindgen]
-pub fn scan_exported_function_names(_input: &[u8]) {
-    alert("Scanning exported function names...");
+pub fn scan_exported_function_names(_input: &[u8]) -> Result<vec::Vec<String>, JsValue> {
+    let mut function_names: vec::Vec<String> = vec![];
+
+    let wat = print_bytes(_input).unwrap();
+    let wat2 = wat::parse_str(&wat).unwrap();
+
+    for payload in Parser::new(0).parse_all(&wat2) {
+        match payload {
+            Ok(p) => {
+                match p {
+                    Payload::ExportSection(export_section) => {
+                        for result in export_section {
+                            match result {
+                                Ok(export) => {
+                                    let first_two_chars: String = export.name.chars().take(2).collect();
+                                    if export.kind == ExternalKind::Func && first_two_chars != "__" {
+                                        function_names.push(export.name.to_string());
+                                    }
+                                }
+                                Err(e) => return Err(JsValue::from_str(&e.to_string())),
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            Err(e) => return Err(JsValue::from_str(&e.to_string())),
+        }
+    }
+    Ok(function_names)
 }
 
 #[wasm_bindgen]
