d1c5f5b5f489e6bf0c324c86b306b70563fffd7c
Luca G.F <luca@quartz.technology>
2024-03-09 18:50:45 +0100

    feat(scanner-core): add scanner core methods implementations (#3)
    Adds scanner implementation with two core methods.
    
    ---------
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>
    Co-authored-by: 0xtekgrinder <0xtekgrinder@protonmail.com>

--- packages/scanner-core/src/lib.rs
+++ packages/scanner-core/src/lib.rs
@@ -1,13 +1,72 @@
 extern crate wasm_bindgen;
 
+use std::u8;
+use std::vec;
+use wasmprinter::print_bytes;
+
 use wasm_bindgen::prelude::*;
+use wasmparser::*;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Scanner {
+    wast: Vec<u8>,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, World!");
+impl Scanner {
+    #[wasm_bindgen(constructor)]
+    pub fn new(input: Vec<u8>) -> Result<Scanner, JsError> {
+        let wat = print_bytes(input.clone()).map_err(|e| JsError::new(&e.to_string()))?;
+        let wast = wat::parse_str(wat).map_err(|e| JsError::new(&e.to_string()))?;
+
+        Ok(Scanner { wast })
+    }
+
+    pub fn exported_function_names(&self) -> Result<vec::Vec<String>, JsError> {
+        let mut exported_function_names: Vec<String> = Vec::new();
+
+        // Iterate through payloads.
+        for payload in Parser::new(0).parse_all(&self.wast) {
+            let payload = payload.map_err(|e| JsError::new(&e.to_string()))?;
+
+            if let Payload::ExportSection(export_section) = payload {
+                // Process each export entry.
+                for export in export_section {
+                    let export = export.map_err(|e| JsError::new(&e.to_string()))?;
+                    let first_two_chars: String = export.name.chars().take(2).collect();
+
+                    if export.kind == ExternalKind::Func && first_two_chars != "__" {
+                        exported_function_names.push(export.name.to_string());
+                    }
+                }
+            }
+        }
+
+        Ok(exported_function_names)
+    }
+
+    pub fn host_functions(&self) -> Result<vec::Vec<String>, JsError> {
+        let mut host_functions: Vec<String> = Vec::new();
+
+        for payload in Parser::new(0).parse_all(&self.wast) {
+            let payload = payload.map_err(|e| JsError::new(&e.to_string()))?;
+
+            match payload {
+                Payload::ImportSection(import_section) => {
+                    for import_item in import_section {
+                        let import_item = import_item.map_err(|e| JsError::new(&e.to_string()))?;
+
+                        if import_item.module == "massa" {
+                            if let TypeRef::Func(_) = import_item.ty {
+                                host_functions.push(import_item.name.to_string());
+                            }
+                        }
+                    }
+                },
+                _ => {}
+            }
+        }
+
+        Ok(host_functions)
+    }
 }
